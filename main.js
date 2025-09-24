14ae80f9c080b55a5f19ca817a9d7516f4918ff7
tavyzin <polarioctavio@gmail.com>
2025-09-24 13:37:29 -0300

    added metrics gathering

--- main.js
+++ main.js
@@ -11,3 +11,34 @@ style.textContent = `
   }
 `;
 document.head.appendChild(style);
+
+const GOOGLE_APS_SCRIPTS_URL = "https://script.google.com/macros/s/AKfycbxEUZf0gxI_EWH5gPN09OKFXyP8dkMOUMAgYtkAyelt6w2UCBm7_b0RzWZhkbYsC0SINA/exec";
+
+function registrarEvento(type) {
+  const payload = {
+    type,
+    dataHora: new Date().toLocaleDateString(),
+    language: Intl.DateTimeFormat().resolvedOptions().timeZone,
+    userAgent: navigator.userAgent
+  };
+
+  fetch(GOOGLE_APS_SCRIPTS_URL, {
+    method: "POST",
+    mode: "no-cors",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(payload)
+  });
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  registrarEvento("VISITAÇÃO");
+});
+
+document.querySelectorAll("a").forEach(link => {
+  link.addEventListener("click", () => {
+    const type = link.dataset.type || "OUTRO";
+    registrarEvento(type);
+  });
+});
